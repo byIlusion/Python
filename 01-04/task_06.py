e6ffc2811db4cf8f594fefcc3395519771af3ddb
Ilusion <by_Ilusion@mail.ru>
2020-06-23 10:35:46 +0700

    Task 06 is complete... :/

--- 01-04/task_06.py
+++ 01-04/task_06.py
@@ -2,8 +2,9 @@ from itertools import count, cycle, takewhile
 
 
 # Вариант А
+print("Вариант A:")
 def my_iterator(start, stop, step=1):
-    """Кастомный итератор с задаваемым диапазоном
+    """Кастомный итератор с задаваемым диапазоном и шагом
 
     start - Стартовое число
     stop - Конечное число
@@ -20,6 +21,7 @@ my_iterator(3, 10)
 
 
 # Вариант B
+print("Вариант B:")
 def my_cycle(itering, count_iterations):
     """Кастомный итератор по циклу
 
@@ -39,24 +41,42 @@ my_cycle(custom_list, 7)
 
 
 # Вариант *A
-def my_iter(start, stop, step=1):
-    iterator = count(start, step)
-    el = next(iterator)
-    while stop >= el:
-        el = next(iterator)
+print("Вариант A*:")
+def my_iter(num, stop, step=1):
+    """Кастомный итератор с задаваемым диапазоном и шагом
+    без "break"
+
+    num - Стартовое число
+    stop - Конечное число
+    step - Шаг итератора
+
+    """
+    iterator = count(num, step)
+    while stop > num:
+        num = next(iterator)
+        print(num)
+
+
+my_iter(13, 20, 3)
+
+
+# Вариант A**
+print("Вариант A**:")
+def my_iter_2(start, stop):
+    """Кастомный итератор с задаваемым диапазоном
+    без "break"
+
+    start - Стартовое число
+    stop - Конечное число
+
+    """
+    iterator = iter(range(start, stop + 1))
+    for el in iterator:
         print(el)
 
 
-my_iter(13, 20)
+my_iter_2(21, 30)
 
 
-# Вариант *B
-def my_Iter_cycle(itering, count_iterations):
-    iterator = cycle(itering)
-    while count_iterations > 0:
-        el = next(iterator)
-        print(el)
-        count_iterations -= 1
-
-
-my_Iter_cycle('abc', 4)
+# Вариант A***
+print(f"Вариант A***:\n{list(takewhile(lambda x: x < 11, count(3)))}")
